6e2c5be8b28e2a0a9d465977af85de6b7c0fadff
infotechbay <161118994+infotechbay@users.noreply.github.com>
2024-04-16 04:21:51 +0500

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,54 @@
+!/usr/bin/env python
+-*- coding: utf-8 -*-
+
+
+install_requires=requires,
+    extras_require=extras,
+    include_package_data=True,
+    license=about["__license__"],
+    classifiers=[
+        Trove classifiers
+        
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: System Administrators",
+    ],
+    tests_require=test_requirements,
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+        "test": PyTest,
+    },
+)
+
+
+Note: To use the 'upload' functionality of this file, you must:
+pipenv install twine --dev
+
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+from setuptools.command.test import test as TestCommand
+
+
+requires = [
+    "python-dateutil==2.8.2",
+    "requests>=2.28.1",
+]
+
+extras = {"dev":["black"]}
+
+test_requirements = [
+    "pytest==7.1.3",
+    "responses==0.21.0",
+]
+
 class UploadCommand(Command):
     """Support setup.py upload."""
 
@@ -33,3 +84,52 @@ class UploadCommand(Command):
         os.system("git push --tags")
 
         sys.exit()
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "pbipy", "__version__.py"), "r", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+with open("README.md", "r", encoding="utf-") as f:
+    readme = f.read()
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    python_requires=">=3.10",
+    url=about["__url__"],
+    packages=find_packages(
+        exclude=[
+            "tests",
+            "*.tests",
+            "*.tests.*",
+            "tests.*",
+            "tests/*"
+        ]
+    ),
+
+
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
