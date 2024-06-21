9ef8c6a756101276391c44aab022969d8caa6a77
infotechbay <161118994+infotechbay@users.noreply.github.com>
2024-06-21 14:42:37 +0500

    Update home.py

--- home.py
+++ home.py
@@ -25,6 +25,53 @@ install_requires=requires,
     },
 )
 
+requires = [
+    "python-dateutil==2.8.2",
+    "requests>=2.28.1",
+]
+
+extras = {"dev":["black"]}
+
+test_requirements = [
+    "pytest==7.1.3",
+    "responses==0.21.0",
+]
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
 
 Note: To use the 'upload' functionality of this file, you must:
 pipenv install twine --dev
