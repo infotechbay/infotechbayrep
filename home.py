e83740d60dcbff639373fa1ad04a8f60b19924cc
infotechbay <161118994+infotechbay@users.noreply.github.com>
2025-03-18 15:46:13 +0500

    Update home.py

--- home.py
+++ home.py
@@ -8,6 +8,54 @@ install_requires=requires,
     license=about["__license__"],
     classifiers=[
         Trove classifiers
+
+requires = [
+    "python-dateutil==2.8.2",
+    "requests>=2.28.1",
+]
+
+extras = {"dev":["black"]}
+
+test_requirements = [
+    "pytest==7.1.3",
+    "responses==0.21.0",
+]
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
         
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
